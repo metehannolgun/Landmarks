a66191ba78748023c88fa0488032721a75b5f30f
metehannolgun <metehannolgun@gmail.com>
2025-03-03 14:32:25 +0300

    Initial Commit

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Metehan Olgun on 3.03.2025.
+//
+
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Metehan Olgun on 3.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
