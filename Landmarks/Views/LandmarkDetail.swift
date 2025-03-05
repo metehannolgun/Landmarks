ce9fa38180b8fef576c73962570017ba8af22907
metehannolgun <metehannolgun@gmail.com>
2025-03-05 13:38:44 +0300

    -Datayı ID'ye göre çekebilmek için Identifable protokolü eklendi

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Metehan Olgun on 5.03.2025.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
